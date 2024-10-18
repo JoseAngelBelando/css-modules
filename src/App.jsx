6f3f6e2ccf72af1678c0d72ae47e0286fec3c37e
joseAngel33 <163841769+joseAngel33@users.noreply.github.com>
2024-10-18 11:56:38 +0200

    +update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+
+import Button from "./components/button/Button";
 import Image from "./components/imagen/Imagen";
 
 const App = ()=> {
-  return <Image/>;
+  return <> 
+      <Image></Image>
+      <Button/>
+      </>  
 };
 
 export default App;
